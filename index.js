9d9ad6fd76044588e76e22378b424f33b1dffe2b
Whizz <joshuaogunsemore7@gmail.com>
2026-05-26 03:24:31 +0100

    Server setup

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const app = require('./src/app');
+const connectDB = require('./src/config/connectDB');
+
+const PORT = process.env.PORT || 4040;
+
+app.listen(PORT, async () => {
+  await connectDB();
+  console.log(`SmartNotes server listening on port ${PORT}`);
+});
