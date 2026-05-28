bf50135f25412854fe61b77035b9b5c701b060e1
Whizz <joshuaogunsemore7@gmail.com>
2026-05-28 14:12:23 +0100

    Added validations

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,11 +2,12 @@ const express = require("express");
 const { healthCheck, registerUser, loginUser, addPicture, setProfile, getProfile, updateProfile } = require("../controllers/user.controller");
 const verifyToken = require("../middlewares/auth");
 const { upload_avatar } = require("../middlewares/upload");
+const { validateLogin, validateRegister } = require("../validations/user.validations");
 const router = express.Router();
 
 router.get('/health', healthCheck);
-router.post('/user/sign-up', registerUser);
-router.post('/user/login', loginUser);
+router.post('/user/sign-up', validateRegister, registerUser);
+router.post('/user/login', validateLogin, loginUser);
 router.post('/user/add-avatar', verifyToken, upload_avatar.single("avatar"), addPicture);
 router.post('/user/profile', verifyToken, setProfile);
 router.get('/user/profile/:userId', verifyToken, getProfile);
---
+++ src/validations/user.validations.js
@@ -0,0 +1,41 @@
+const Joi = require('joi');
+
+
+const registerSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required()
+});
+
+const loginSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required()
+});
+
+const validateRegister = (req, res, next) =>{
+    const {error} = registerSchema.validate(req.body);
+
+    if(error){
+        return res.status(400).json({
+            error: error.details[0].message
+        });
+    }
+
+    next();
+}
+
+const validateLogin = (req, res, next) =>{
+    const {error} = loginSchema.validate(req.body);
+
+    if(error){
+        return res.status(400).json({
+            error: error.details[0].message
+        });
+    }
+
+    next();
+}
+
+module.exports = {
+    validateRegister,
+    validateLogin
+}
